6faf4f28c57c0b264f63732cb36de97769e4a042
Max <Kieran has a weird sense of humour>
2019-11-06 16:08:17 +0000

    second commit

--- First.js
+++ First.js
@@ -1,30 +1,77 @@
-var message = "shafeeq is back is shafeeq no";
-var checkedword = [];
-var usedword = [];
-var newsentence = "";
-var word = "";
+// class data{
+//     constructor(){
+//         this.something={
+//             "regno":"24",
+//             "name":"Shafeeq"
+//         }
+//     }
+//     adding(Key1,Value1){
+//         this.something[Key1]=Value1;
+//     }
+// }
+// var ref=new data(); 
+// ref.adding("postcode","m234");
+// ref.adding("regno","50");
 
-for(var i = 0; i<message.length; i++){
-    if(message.substring(i,i+1)==" "){
-        for(var j=0; j<message.length; j++){
-            if(word==usedword[j]){
-                word="";
+// console.log(ref.something["postcode"]);
+// console.log(ref.something["regno"]);
+
+
+// class nationwide{
+//     constructor(){
+//         this.records={}
+//     }
+//     adding(Key,ValueFunction){
+//         this.records.push[Key]=ValueFunction;
+//     }
+// }
+// var ref=new nationwide();
+// ref.adding(records.push("training"),function(){
+//     console.log("do study");
+// })
+
+// var refevents = require("events");
+// var emitter = new refevents.EventEmitter();
+
+// emitter.on("boom", function(){
+//     console.log("hello");
+// });
+// emitter.on("file", function(){
+//     console.log("Hello");
+// });
+// emitter.emit("boom");
+// emitter.emit("file");
+
+// var count = 0;
+// var FS = require("fs");
+// var F= FS.createReadStream("file.txt",{
+//     encoding:'utf8'})
+//     F.on('data',function(text){
+//         count+=1
+//        console.log(text);
+//    });
+//    F.on('end',function(){
+//        console.log(count);
+//        console.log("File finished");
+//    });
+
+var count = 0;
+var FS = require("fs");
+var F2= FS.createWriteStream("file8.txt");
+var F=FS.createReadStream("file.txt",{encoding:'utf8'})
+
+    F.on('data',function(text){
+        count+=1
+        for(var i = 0; i<text.length; i++){
+            if (text.substring(i,i+1)=="e"){
+                F2.write("!!!!!!!!!!!!!!!");
             }
-
-        }
-        if(word !== ""){
-            newsentence+=word + " ";
-            usedword.push(word);
-            word = "";
-        }
-
-
-
+            else{
+                F2.write(text.substring(i,i+1))
+            }
     }
-    else{
-        word+=message.substring(i,i+1);
-
-    }
-}
-console.log(usedword);
-console.log(newsentence + word);
+   })
+   F.on('end',function(){
+       console.log(count);
+       console.log("File finished");
+   });
